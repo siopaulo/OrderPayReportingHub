7bb9da9d0af7c7279dcf738dd74af4cba9b55047
siopaulo <pavlicekvr@gmail.com>
2026-02-23 19:52:00 +0100

    feat: maven init

---
+++ src/main/java/cz/pavli/OrderPayReportingHub/OrderPayReportingHubApplication.java
@@ -0,0 +1,13 @@
+package cz.pavli.OrderPayReportingHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPayReportingHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPayReportingHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/pavli/OrderPayReportingHub/OrderPayReportingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.pavli.OrderPayReportingHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPayReportingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
